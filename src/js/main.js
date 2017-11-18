f1564c562ec32e068cc3fb7eb231e808862350e6
Dmitry1101 <dmitrii.dmitriev.2017@mail.ru>
2017-11-18 05:05:41 +0300

    final 17.11.17

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-		
+	
+	$('body').addClass('loaded');
+
 	function setEqualHeight( objj ) {
 
 	   var maxHeight = -1;
@@ -17,19 +19,31 @@ $(document).ready(function(){
 
 	setEqualHeight('.testim__cont-1 .testim__txt');
 	setEqualHeight('.testim__cont-2 .testim__txt');
-	// setEqualHeight('.testim__cont-2 .testim__txt');
+	
 
 	$(window).resize(function(){
 	  setEqualHeight('.testim__cont-1 .testim__txt');
 		setEqualHeight('.testim__cont-2 .testim__txt');
 	});
 
-});
+
+	//ACCORDEON
+	(function () {
+		var acc = document.getElementsByClassName("accordion");
+		var i;
+
+		for (i = 0; i < acc.length; i++) {
+		  acc[i].onclick = function() {
+		    this.classList.toggle("active");
+		    var panel = this.nextElementSibling;
+		    if (panel.style.maxHeight){
+		      panel.style.maxHeight = null;
+		    } else {
+		      panel.style.maxHeight = panel.scrollHeight + "px";
+		    } 
+		  }
+		}
+	})(); 
 
 
-
-
-
-$(window).on('load',function(){
-	$('body').addClass('loaded');
 });

1b0c229049d0fa7b77f0d0b492dc72edd5291c67
Dmitrii Krapivko <dmitrii1101@bk.ru>
2017-11-21 11:03:23 +0300

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -44,6 +44,47 @@ $(document).ready(function(){
 		  }
 		}
 	})(); 
+	
+	$('body').on('click', '.morre', function(){
+		
+		var httml = '<div class="blog__item" style="background: url(\'img/blog-9.jpg\') center no-repeat; -webkit-background-size: cover; background-size: cover;">' +
+							'<div class="blog__img">' +
+								'<img src="img/blog-9.jpg" alt="">' +
+							'</div>'	+					
+							'<div class="blog__content">' +
+								'<div class="blog__date">Oct 14, 2017 | 0 comments</div>' +
+								'<div class="blog__content-title">' +
+									'Key Information On Monaco Visa Requirements – Visa Reservation' +
+								'</div>' +
+								'<div class="blog__content-txt">' +
+									'<p>One of the smallest countries in the world, Monaco is an entire nation squeezed into a few hectares of land. But, make no mistake; this micronation, just outside France, is an ultimate t…</p>' +
+								'</div>' +
+								'<p class="blog__content-read">' +
+									'<a href="#">Read More ></a>' +
+								'</p>' +
+							'</div>' +
+						'</div>' +
+
+						'<div class="blog__item" style="background: url(\'img/blog-8.jpg\') center no-repeat; -webkit-background-size: cover; background-size: cover;">' +
+							'<div class="blog__img">' +
+								'<img src="img/blog-8.jpg" alt="">' +
+							'</div>'	+					
+							'<div class="blog__content">' +
+								'<div class="blog__date">Oct 14, 2017 | 0 comments</div>' +
+								'<div class="blog__content-title">' +
+									'Key Information On Monaco Visa Requirements – Visa Reservation' +
+								'</div>' +
+								'<div class="blog__content-txt">' +
+									'<p>One of the smallest countries in the world, Monaco is an entire nation squeezed into a few hectares of land. But, make no mistake; this micronation, just outside France, is an ultimate t…</p>' +
+								'</div>' +
+								'<p class="blog__content-read">' +
+									'<a href="#">Read More ></a>' +
+								'</p>' +
+							'</div>' +
+						'</div>';
+
+		$('.blog__list').append(httml);
+	});
 
 
 });
